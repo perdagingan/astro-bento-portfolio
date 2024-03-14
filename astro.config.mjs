be1d554a4f4dd93bf5118a2e7f54e78b8e0ace94
anamfathonaya <050109245@ecampus.ut.ac.id>
2024-03-14 13:09:01 +0700

    perubahan-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,24 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gianmarco.xyz/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon()
-  ],
+  integrations: [sitemap(), robotsTxt({
+    sitemap: ["https://gianmarco.xyz/sitemap-index.xml", "https://gianmarco.xyz/sitemap-0.xml"]
+  }), solidJs(), UnoCSS({
+    injectReset: true
+  }), icon()],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
---
+++ src/pages/gallery/post-1.md
@@ -0,0 +1,11 @@
+---
+
+layout: "../../layouts/ImageDetail.astro"
+title: Centering
+date: 2020-03-05
+image: {
+  src: "../../assets/IMG-1.jpeg"
+}
+description: Photo of three BQAM students in the middle of a balcony railing iron frame and concrete foundation of a male dormitory project.
+
+---
